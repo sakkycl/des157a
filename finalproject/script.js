481bed263750bec49350a9645d94f5d6fc8c830b
Sally Lee <slllee@ucdavis.edu>
2024-03-16 16:26:48 -0700

    edit final project comments in js

--- finalproject/script.js
+++ finalproject/script.js
@@ -14,6 +14,7 @@
     const requiredMovement = 1;
     let initialX = null;
 
+    //swipe mouse interaction
     turnPage.addEventListener('mouseover', function(event){
         
         const { clientX } = event;
